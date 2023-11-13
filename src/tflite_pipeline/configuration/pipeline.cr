af980d1b29c83ec9a1c9be3ed83411f6cbb59f49
Stephen von Takach <steve@place.technology>
2023-11-14 09:27:25 +1100

    chore(config/pipeline): remove unused property

--- src/tflite_pipeline/configuration/pipeline.cr
+++ src/tflite_pipeline/configuration/pipeline.cr
@@ -4,8 +4,6 @@ class TensorflowLite::Pipeline::Configuration::Pipeline
   include JSON::Serializable
   include YAML::Serializable
 
-  property index : Int32? = nil
-
   property name : String
   property description : String?
 
